cb3312e16c774f62122e27e6be6bd1aa94a268ea
Tomas Smulkys <tsmulkys@gmail.com>
2018-03-23 16:01:09 +0200

    Fix toolbar articles categories and sub-categories order

--- app/Helpers/Categories.php
+++ app/Helpers/Categories.php
@@ -1,7 +1,13 @@
 <?php
 
 function getAllCategoriesWithSubCategories(){
-	$categories = \App\Category::select('id', 'name')->with('subCategories:id,name,category_id')->orderBy('level', 'desc')->get();
+    $with = [
+        'subCategories' => function($query){
+            $query->select('id', 'name', 'category_id')->orderBy('level', 'desc');
+        }
+    ];
+    
+	$categories = \App\Category::select('id', 'name')->with($with)->orderBy('level', 'desc')->get();
 	
 	return json_encode($categories->toArray());
 }
